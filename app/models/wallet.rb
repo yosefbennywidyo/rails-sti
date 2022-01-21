6bd52b94bc55053b2087bf0a05eff4ca92374407
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-22 06:59:19 +0800

    Update wallet model: - add outgoing_transactions and incoming_transactions relationship

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,5 @@
 class Wallet < ApplicationRecord
   belongs_to :client
+  has_many :outgoing_transactions, foreign_key: 'sender_wallet_id', class_name: 'Transaction'
+  has_many :incoming_transactions, foreign_key: 'receiver_wallet_id', class_name: 'Transaction'
 end
