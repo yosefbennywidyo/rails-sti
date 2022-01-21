9351a7bbc2d39c8f6c17119eed679ba212d95cac
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-22 06:52:05 +0800

    Update client model: - add validation for email and name - add relation with wallet and transactions - add method to check if a Client is user?, team?, or stock?

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,21 @@
 class Client < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
+
+  has_one :wallet
+  has_many :transactions
+
+  def user?
+    type.eql?('User')
+  end
+
+  def team?
+    type.eql?('Team')
+  end
+
+  def stock?
+    type.eql?('Stock')
+  end
 end
