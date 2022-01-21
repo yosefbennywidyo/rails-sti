9b5b5e42107f41697a01045e532b604df2569a85
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-22 07:51:34 +0800

    Update create wallet migration: - change balance to integer - change references to belongs_to - change null: false to index: true

--- db/migrate/20220121061209_create_wallets.rb
+++ db/migrate/20220121061209_create_wallets.rb
@@ -1,8 +1,8 @@
 class CreateWallets < ActiveRecord::Migration[7.0]
   def change
     create_table :wallets do |t|
-      t.string :balance
-      t.references :client, null: false, foreign_key: true
+      t.integer :balance
+      t.belongs_to :client, index: true, foreign_key: true
 
       t.timestamps
     end
