8c55ef74524c45d5188baab611298af4dc8e5fa0
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-22 06:58:27 +0800

    Update create transaction migration: - add type, so Credit and Debit can inherit from Transaction - add relations - change amount to integer

--- db/migrate/20220121062210_create_transactions.rb
+++ db/migrate/20220121062210_create_transactions.rb
@@ -1,7 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
-      t.string :amount
+      t.integer :amount
+      t.string :type
+      t.belongs_to :client
+      t.belongs_to :sender_wallet#, foreign_key: { to_table: :wallets }
+      t.references :receiver_wallet#, foreign_key: { to_table: :wallets }
 
       t.timestamps
     end
