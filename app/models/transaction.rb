995b028a214f3b39e6c9bbb911185b4b6598aba2
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-23 04:02:08 +0800

    Update transaction model: - add numericality validation - add rename scope for better read - remove cache_setup method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
+  validates :amount, :numericality => {:only_integer => true}
   before_create :check_sender_wallet_balance, unless: :credit?
   after_create :update_sender_and_receiver_wallet_balance, if: :debit?
   after_create :credit_wallet_balance, if: :credit?
@@ -10,9 +11,11 @@ class Transaction < ApplicationRecord
   belongs_to :sender_wallet, class_name: 'Wallet', optional: true
   belongs_to :receiver_wallet, class_name: 'Wallet', optional: true
 
-  scope :incoming_transactions, -> (user_wallet) { where(receiver_wallet_id: user_wallet.id).where(type: 'Credit').sum(:amount) }
-  scope :outgoing_transactions, -> (user_wallet) { where(sender_wallet_id: user_wallet.id).where("amount < ?", 0).sum(:amount) }
-  scope :balance, -> (user_wallet) { incoming_transactions(user_wallet) + outgoing_transactions(user_wallet) }
+  scope :credit, -> (user_wallet) { where(receiver_wallet_id: user_wallet.id).where(type: 'Credit') }
+  scope :debit, -> (user_wallet) { where(sender_wallet_id: user_wallet.id).where("amount < ?", 0) }
+  scope :total_credit, -> (user_wallet) { where(receiver_wallet_id: user_wallet.id).where(type: 'Credit').sum(:amount) }
+  scope :total_debit, -> (user_wallet) { where(sender_wallet_id: user_wallet.id).where("amount < ?", 0).sum(:amount) }
+  scope :balance, -> (user_wallet) { total_credit(user_wallet) + total_debit(user_wallet) }
 
   def setup_sender_receiver_wallet
     @sender_wallet = self.sender_wallet
@@ -26,37 +29,18 @@ class Transaction < ApplicationRecord
     end
   end
 
-  def cache_setup(user_wallet)
-    @cache = Rails.cache
-    
-    @cache.fetch("incoming_transactions_for_user_with_id_#{user_wallet.client_id}") do
-      Transaction.incoming_transactions(user_wallet)
-    end
-    
-    @cache.fetch("outgoing_transactions_for_user_with_id_#{user_wallet.client_id}") do
-      Transaction.outgoing_transactions(user_wallet)
-    end
-    
-    @cache.fetch("balance_for_user_with_id_#{user_wallet.client_id}") do
-      Transaction.balance(user_wallet)
-    end
-  end
-
   def update_sender_and_receiver_wallet_balance
     @sender_wallet.update(balance: @sender_wallet.balance + self.amount)
-    cache_setup(@sender_wallet)
 
     if @receiver_wallet.present?
       @receiver_wallet.update(balance: @receiver_wallet.balance.to_i - self.amount)
       Credit.create(client_id: self.client_id, amount: self.amount * -1, receiver_wallet: @receiver_wallet, sender_wallet: @sender_wallet)
-      cache_setup(@receiver_wallet)
     end
   end
 
   def credit_wallet_balance
     setup_sender_receiver_wallet
     @receiver_wallet.update(balance: @receiver_wallet.balance.to_i + self.amount)
-    cache_setup(@receiver_wallet)
   end
 
   def credit?
