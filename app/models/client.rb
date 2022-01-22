54a860a4de188677aa911dee4cfd55180530e43a
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-23 03:57:20 +0800

    Update client model: - add dependent: destroy - add exclude scope - add users, teams, stocks methods

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,9 +4,36 @@ class Client < ApplicationRecord
   validates :email, uniqueness: true
   validates :name, uniqueness: true
 
-  has_one :wallet
+  has_one :wallet, dependent: :destroy
   has_many :transactions
 
+  scope :exclude, -> (*values) { 
+    where(
+      "#{table_name}.id NOT IN (?)",
+        (
+          values.compact.flatten.map { |e|
+            if e.is_a?(Integer) 
+              e
+            else
+              e.is_a?(self) ? e.id : raise("Element not the same type as #{self}.")
+            end
+          } << 0
+        )
+      )
+    }
+
+  def self.users
+    where(type: 'User')
+  end
+
+  def self.teams
+    where(type: 'Team')
+  end
+
+  def self.stocks
+    where(type: 'Stock')
+  end
+  
   def user?
     type.eql?('User')
   end
