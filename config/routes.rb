81b1514cd1ba51de0328755fbae58fcf4687647a
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-23 03:19:11 +0800

    Setup routes: - root to clients index - add credit, create_credit, debit, create_debit   and add transactions resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :clients
+  get "clients/:client_id/credit", to: "transactions#credit", as: 'credit_transaction'
+  post "clients/:client_id/create_credit", to: "transactions#create_credit", as: 'create_credit_transaction'
+  get "clients/:client_id/debit", to: "transactions#debit", as: 'debit_transaction'
+  post "clients/:client_id/create_debit", to: "transactions#create_debit", as: 'create_debit_transaction'
+  
+  resources :clients do
+    resources :transactions, only: [:index]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "clients#index"
 end
